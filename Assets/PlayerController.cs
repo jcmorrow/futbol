c0a481f5a99c83b4dce31d256cc8344e7874165b
Josh Morrow <jcmorrow@thoughtbot.com>
2016-09-07 22:35:55 -0700

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	public float speed;
+	public float turnSpeed;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		RenderSettings.ambientLight = Color.black;
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (0, 0, moveVertical);
+
+		rb.AddRelativeForce (movement * speed);
+	}
+
+	void Update () {
+		rb.AddTorque(new Vector3(0.0f, Input.GetAxis ("Horizontal") * turnSpeed, 0.0f));
+	}
+}
